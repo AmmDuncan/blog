e2be2e99d6b56d6768a4b4dbcc31014e43b2d6b0
Ammiel <ammiel@Ammiels-MacBook-Pro-3.local>
2024-03-10 17:08:05 +0000

    fix: bring navbar forward on z axis

--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 export function Navbar() {
   return (
-    <div className="sticky top-0 z-10 flex h-16 items-center border-b border-b-purple-300/70 bg-purple-400 px-8 text-white shadow-[0_1rem_3.2rem_rgba(36,36,53,.05)]">
+    <div className="sticky top-0 z-[100] flex h-16 items-center border-b border-b-purple-300/70 bg-purple-400 px-8 text-white shadow-[0_1rem_3.2rem_rgba(36,36,53,.05)]">
       <Link href="/" className="flex items-center gap-2">
         <Logo />
         <Text>Ammiel Yawson</Text>
