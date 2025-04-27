9a313b2756b734518e3358bbbcf60be5bbad7dc1
Ammiel Yawson <ammielgyanyawson@gmail.com>
2025-04-27 22:46:11 +0000

    Remove greeting header from blog landing page

--- src/app/_components/BlogHeader.tsx
+++ src/app/_components/BlogHeader.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export const BlogHeader: React.FC = () => (
   <header className="container mx-auto py-20 text-center">
-    <h1 className="mb-4 font-serif text-4xl text-[var(--color-text)] md:text-5xl">
+    {/* <h1 className="mb-4 font-serif text-4xl text-[var(--color-text)] md:text-5xl">
       Hi there,
-    </h1>
+    </h1> */}
     <p className="mx-auto max-w-md font-sans text-xl text-[var(--color-text)]">
       Welcome to my space. An open look into insights from my journey.
     </p>
