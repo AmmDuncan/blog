a21abe1a6ed874cc31e51f074c3d2b0a734eae53
Ammiel Yawson <ammielgyanyawson@gmail.com>
2025-06-24 01:49:20 +0000

    fix: add optional chaining to post image handling in usePostImage hook

--- src/hooks/usePostImage.ts
+++ src/hooks/usePostImage.ts
@@ -2,13 +2,13 @@ import { useTheme } from '@/context/theme';
 import { Post } from 'contentlayer/generated';
 import { useMemo } from 'react';
 
-export function usePostImage(post: Post) {
+export function usePostImage(post?: Post) {
   const { theme } = useTheme();
   const featureImage = useMemo(() => {
-    if (theme === 'dark' && post.feature_dark_mode_image) {
+    if (theme === 'dark' && post?.feature_dark_mode_image) {
       return post.feature_dark_mode_image;
     }
-    return post.feature_image;
+    return post?.feature_image;
   }, [theme, post]);
 
   return { featureImage };
