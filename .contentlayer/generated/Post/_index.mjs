3672f70f8799d728a1edda0b299658a7ab2cba61
Ammiel Yawson <ammielgyanyawson@gmail.com>
2025-07-02 13:36:51 +0000

    feat: add new blog post on structuring frontend projects for maintainability success

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -4,8 +4,9 @@ import _3JavascriptStringMethodsYoullUseOftenAndWhereYoullNeedThemMdx from './_3
 import provideInjectHowToDrillPropsInVueWithoutActuallyPropDrillingMdx from './provide-inject-how-to-drill-props-in-vue-without-actually-prop-drilling.mdx.json' assert { type: 'json' }
 import recipeForAnEffectiveWebsiteMdx from './recipe-for-an-effective-website.mdx.json' assert { type: 'json' }
 import stopUsingUseeffectForEverythingTryLazyEffectsInsteadMdx from './stop-using-useeffect-for-everything-try-lazy-effects-instead.mdx.json' assert { type: 'json' }
+import structuringFrontendProjectsForMaintainabilitySuccessMdx from './structuring-frontend-projects-for-maintainability-success.mdx.json' assert { type: 'json' }
 import theCssBoxModelTheFoundationOfLayoutsOnTheWebMdx from './the-css-box-model-the-foundation-of-layouts-on-the-web.mdx.json' assert { type: 'json' }
 import thePaginationBugHidingInPlainSightMdx from './the-pagination-bug-hiding-in-plain-sight.mdx.json' assert { type: 'json' }
 import webDevelopmentYourFriendlyEntryToTheTechCommunityIn_2022Mdx from './web-development-your-friendly-entry-to-the-tech-community-in-2022.mdx.json' assert { type: 'json' }
 
-export const allPosts = [_3JavascriptStringMethodsYoullUseOftenAndWhereYoullNeedThemMdx, provideInjectHowToDrillPropsInVueWithoutActuallyPropDrillingMdx, recipeForAnEffectiveWebsiteMdx, stopUsingUseeffectForEverythingTryLazyEffectsInsteadMdx, theCssBoxModelTheFoundationOfLayoutsOnTheWebMdx, thePaginationBugHidingInPlainSightMdx, webDevelopmentYourFriendlyEntryToTheTechCommunityIn_2022Mdx]
+export const allPosts = [_3JavascriptStringMethodsYoullUseOftenAndWhereYoullNeedThemMdx, provideInjectHowToDrillPropsInVueWithoutActuallyPropDrillingMdx, recipeForAnEffectiveWebsiteMdx, stopUsingUseeffectForEverythingTryLazyEffectsInsteadMdx, structuringFrontendProjectsForMaintainabilitySuccessMdx, theCssBoxModelTheFoundationOfLayoutsOnTheWebMdx, thePaginationBugHidingInPlainSightMdx, webDevelopmentYourFriendlyEntryToTheTechCommunityIn_2022Mdx]
